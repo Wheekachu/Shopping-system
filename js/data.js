4f14cfc9c80bc142650127226a6f3812096a1cad
Nancy Sy <synancy0926@gmail.com>
2023-04-19 23:58:11 +0800

    transfer all files

--- js/data.js
+++ js/data.js
@@ -29,6 +29,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
 //products
 
 fetch('data.json')
@@ -37,6 +38,7 @@ fetch('data.json')
 })
 .then(function (data) {
 	appendData(data);
+  addToCart(data);
 })
 .catch(function (err) {
 	console.log('error: ' + err);
@@ -46,65 +48,65 @@ var mainContainer = document.getElementById("myData");
 for (var i = 0; i < data.length; i++) {
 
 	 var div = document.createElement("div");
-	 div.innerHTML = '<img class="imageProd" src="'+data[i].image+'">' 
-	 +'<p class="Prodname">Name: <b>' + data[i].name + '</b>'
+	 div.innerHTML = '<img class="imageProd" value="'+data[i].image+'" src="'+data[i].image+'">' 
+	 +'<p class="Prodname" id="Prodname" value="'+data[i].name+'">Name: <b>' + data[i].name + '</b>'
 	 +'<br>Price: <b>P' + data[i].price + '</b></p>'
-	 +'<a href="#'+ data[i].id +'" class="button-right">Place Order </a>';
+   +'<a href="#'+ data[i].id +'" onclick="addToCart('+data[i].id+')" class="btn button-right">Place Order </a>'
 	mainContainer.appendChild(div);
 }
 }
-// 
-// function showTable(){
-// 	fetch("data.json")
-// 	  .then(response => response.json())
-// 	  .then(data => createTable(data));
-//   }
-	  
-//   function createTable(data) {
-//   var table = "<table border=1>";
-//   // add a row for name and marks
-//   table += `<tr>
-// 			  <th>Name</th>
-// 			  <th colspan="4">Marks</th>
-// 			</tr>`;
-//   // now add another row to show subject
-//   table += `<tr>
-// 			  <th></th>
-// 			  <th>Math</th>
-// 			  <th>English</th>
-// 			  <th>Chemistry</th>
-// 			  <th>Physics</th>
-// 			</tr>`;
-//   // now loop through students
-//   // show their name and marks
-//   var tr = "";
-//   for(let i = 0; i < data.product.length; i++) {
-// 	tr += "<tr>";
-// 	tr += `<td>${data.product[i].name}</td>`;
-// 	for (var key in data.product[i].id) {
-// 	  tr += `<td>${data.product[i].id[key]}</td>`;
-// 	}
-// 	tr += "</tr>"
-//   }
-//   table += tr + "</table>";
-  
-// 	// append table to body
-// 	document.body.innerHTML += table;
-//   }
 
-//auto slideshow
-// let slideIndex = 0;
-// showSlides();
+function addToCart(prod) {
+  fetch('data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    getData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+function getData(data) {
+  var container = document.getElementById("modal-body");
+  for(let i = 0; i<data.length; i++){
+        if (data[i].id === prod){
+          var table = document.createElement("table");
+          table.innerHTML = '<tr><td><img style="width: 300px; height: 200px; border-radius: 2%;'
+          +'box-shadow: 0 5px 10px #9c9999;" value="'+data[i].image+'" src="'+data[i].image+'"></td>' 
+          +'<td><p value="'+data[i].name+'">Name: <b>' + data[i].name + '</b>'
+          +'<br>Price: <b>P' + data[i].price + '</b></p></td></tr>'
+         container.appendChild(table);
+          console.log(data[i].name);
+        }
+    }
+  }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
